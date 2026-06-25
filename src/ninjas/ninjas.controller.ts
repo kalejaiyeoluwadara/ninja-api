6202abc3e73fa3e0bb8d36578eb2b03eaa98bced
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2026-06-25 19:37:52 +0100

    Implement CRUD operations for ninjas in the NinjasController and NinjasService, including methods to create, read, update, and delete ninjas, along with filtering by weapon type.

--- src/ninjas/ninjas.controller.ts
+++ src/ninjas/ninjas.controller.ts
@@ -1,18 +1,37 @@
-import { Controller, Get, Param, Post } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 import { Body } from '@nestjs/common';
+import { CreateNinjaDto } from './dto/create-ninja.dto';
+import { NinjasService } from './ninjas.service';
+import { UpdateNinjaDto } from './dto/update-ninja.dto';
 
 @Controller('ninjas')
 export class NinjasController {
 
+    constructor(private readonly ninjasService: NinjasService) {}
+
     @Get()
-    getNinjas() {
-        return [];
+    getNinjas(@Query('weapon') weapon: 'stars' | 'nunchucks') {
+        return this.ninjasService.getAllNinjas(weapon);
     }
 
     @Get(':id')
-    getNinja(@Param('id') id: string) {
-        return `getNinja ${id}`;
+    getNinja(@Param('id') id: number) {
+        return this.ninjasService.getNinjaById(id);
     }
 
+    @Patch(':id')
+    updateNinja(@Param('id') id: number, @Body() updateNinjaDto: UpdateNinjaDto) {
+        return this.ninjasService.updateNinja(id, updateNinjaDto);
+    }
+
+    @Post()
+    createNinja(@Body() createNinjaDto: CreateNinjaDto) {
+        return this.ninjasService.createNinja(createNinjaDto);
+    }
+
+    @Delete(':id')
+    deleteNinja(@Param('id') id: number) {
+        return this.ninjasService.deleteNinja(id);
+    }
   
 }
--- src/ninjas/ninjas.service.ts
+++ src/ninjas/ninjas.service.ts
@@ -1,4 +1,44 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateNinjaDto } from './dto/create-ninja.dto';
+import { UpdateNinjaDto } from './dto/update-ninja.dto';
 
 @Injectable()
-export class NinjasService {}
+export class NinjasService {
+    private ninjas = [
+        { id: 0, name: 'Ninja A', weapon: 'stars' },
+        { id: 1, name: 'Ninja B', weapon: 'nunchucks' },
+        { id: 2, name: 'Ninja C', weapon: 'stars' },
+    ];
+    getAllNinjas( weapon?: 'stars' | 'nunchucks' ) {
+        if (weapon) {
+            return this.ninjas.filter((ninja) => ninja.weapon === weapon);
+        }
+        return this.ninjas;
+    }   
+
+    getNinjaById(id: number) {
+        const ninja = this.ninjas.find((ninja) => ninja.id === id);
+        if (!ninja) throw new NotFoundException('Ninja not found');
+        return ninja;
+    }
+
+    updateNinja(id: number, updateNinjaDto: UpdateNinjaDto) {
+        const ninja = this.getNinjaById(id);
+        ninja.name = updateNinjaDto.name ?? ninja.name;
+        ninja.weapon = updateNinjaDto.weapon ?? ninja.weapon;
+        return ninja;
+    }
+
+    deleteNinja(id: number) {
+        const index = this.ninjas.findIndex((ninja) => ninja.id === id);
+        if (index === -1) throw new NotFoundException('Ninja not found');
+        this.ninjas.splice(index, 1);
+        return { message: 'Ninja deleted successfully' };
+    }
+
+    createNinja(createNinjaDto: CreateNinjaDto) {
+        const newNinja = { id: this.ninjas.length, name: createNinjaDto.name, weapon: createNinjaDto.weapon };
+        this.ninjas.push(newNinja);
+        return newNinja;
+    }
+}
